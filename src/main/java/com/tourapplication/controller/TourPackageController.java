9349f1f35136a9542d2e8e2248ae019e7d7253c6
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-23 17:20:22 +0530

    Add files via upload

---
+++ src/main/java/com/tourapplication/controller/TourPackageController.java
@@ -0,0 +1,96 @@
+package com.tourapplication.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tourapplication.dto.TourPackageDTO;
+import com.tourapplication.service.TourPackageService;
+
+import javassist.NotFoundException;
+
+@RestController
+@RequestMapping("/api/packages")
+public class TourPackageController {
+
+	private final TourPackageService packageService;
+
+	@Autowired
+	public TourPackageController(TourPackageService packageService) {
+		this.packageService = packageService;
+	}
+
+	@GetMapping
+	public ResponseEntity<Page<TourPackageDTO>> getAllPackagesPaged(@RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "10") int size) {
+		Page<TourPackageDTO> packages = packageService.getAllPackages(PageRequest.of(page, size, Sort.by("placeName")));
+		return new ResponseEntity<>(packages, HttpStatus.OK);
+	}
+
+	@GetMapping("/by-days")
+	public ResponseEntity<List<TourPackageDTO>> getAllPackagesByNumberOfDays(@RequestParam int numberOfDays) {
+		List<TourPackageDTO> packages = packageService.getAllPackagesByNumberOfDays(numberOfDays);
+		return new ResponseEntity<>(packages, HttpStatus.OK);
+	}
+
+	@GetMapping("/by-user/{userId}")
+	public ResponseEntity<List<TourPackageDTO>> getAllPackagesByUser(@PathVariable Long userId) {
+		List<TourPackageDTO> packages = packageService.getAllPackagesByUser(userId);
+		return new ResponseEntity<>(packages, HttpStatus.OK);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<TourPackageDTO> getPackageById(@PathVariable Long id) {
+		TourPackageDTO tourPackageDTO;
+		try {
+			tourPackageDTO = packageService.getPackageById(id);
+			return new ResponseEntity<>(tourPackageDTO, HttpStatus.OK);
+		} catch (NotFoundException e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@PostMapping
+	public ResponseEntity<TourPackageDTO> createPackage(@RequestBody @Valid TourPackageDTO tourPackageDTO) {
+		TourPackageDTO createdPackage = packageService.createPackage(tourPackageDTO);
+		return new ResponseEntity<>(createdPackage, HttpStatus.CREATED);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<TourPackageDTO> updatePackage(@PathVariable Long id,
+			@RequestBody @Valid TourPackageDTO tourPackageDTO) {
+		TourPackageDTO updatedPackage;
+		try {
+			updatedPackage = packageService.updatePackage(id, tourPackageDTO);
+			return new ResponseEntity<>(updatedPackage, HttpStatus.OK);
+		} catch (NotFoundException e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deletePackageById(@PathVariable Long id) {
+		try {
+			packageService.deletePackageById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (NotFoundException e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+}
---
+++ src/main/java/com/tourapplication/dto/TourPackageDTO.java
@@ -0,0 +1,93 @@
+package com.tourapplication.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+
+import com.tourapplication.entity.User;
+
+public class TourPackageDTO {
+	private Long id;
+
+	@NotBlank(message = "Place name is required")
+	private String placeName;
+
+	@Positive
+	private int numberOfDays;
+
+	@Positive
+	private double price;
+
+	private User user;
+
+	@NotBlank(message = "Places to visit should be provided")
+	private String placesToVisit;
+
+	public TourPackageDTO() {
+		super();
+	}
+
+	public TourPackageDTO(Long id, @NotBlank(message = "Place name is required") String placeName, int numberOfDays,
+			double price, @NotBlank(message = "Places to visit should be provided") String placesToVisit, User user) {
+		super();
+		this.id = id;
+		this.placeName = placeName;
+		this.numberOfDays = numberOfDays;
+		this.price = price;
+		this.placesToVisit = placesToVisit;
+		this.user = user;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPlaceName() {
+		return placeName;
+	}
+
+	public void setPlaceName(String placeName) {
+		this.placeName = placeName;
+	}
+
+	public int getNumberOfDays() {
+		return numberOfDays;
+	}
+
+	public void setNumberOfDays(int numberOfDays) {
+		this.numberOfDays = numberOfDays;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getPlacesToVisit() {
+		return placesToVisit;
+	}
+
+	public void setPlacesToVisit(String placesToVisit) {
+		this.placesToVisit = placesToVisit;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public String toString() {
+		return "TourPackageDTO [id=" + id + ", placeName=" + placeName + ", numberOfDays=" + numberOfDays + ", price="
+				+ price + ", user=" + user + ", placesToVisit=" + placesToVisit + "]";
+	}
+}
